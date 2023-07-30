5e220369eea8b23fa91236afa41669f4d59bcadd
catraboom <124100757+catraboom@users.noreply.github.com>
2023-07-30 23:44:59 +1000

    Updated ai (last minute changes)

--- Main Game/Pygame/maingameai.py
+++ Main Game/Pygame/maingameai.py
@@ -6,15 +6,18 @@ GRID_SIZE = 11 # Size of the grid (11x11)
 CELL_SIZE = 50 # Size of each cell (50x50 pixels)
 PADDING_TOP = 50 # Padding on top of the grid (50 pixels)
 WINDOW_SIZE = (GRID_SIZE * CELL_SIZE, (GRID_SIZE * CELL_SIZE) + PADDING_TOP)
-PLAYER_COLOR = (66, 73, 255)  # Blue Player colour
-AI_COLOR = (255, 66, 66)  # Red AI colour
+PLAYER_COLOUR = (66, 73, 255)  # Blue Player colour
+AI_COLOUR = (255, 66, 66)  # Red AI colour
 FONT_SIZE = 20 # Font size (20 pixels)
-FONT_COLOR = (0, 0, 0)  # Black font colour
-FONT_COLOR_2 = (255, 255, 255)  # White font colour
+FONT_COLOUR = (0, 0, 0)  # Black font colour
+FONT_COLOUR_2 = (255, 255, 255)  # White font colour
 CUSTOM_FONT_FILE = 'Main Game\Fonts\Minecraftia.ttf' # Custom font file
 PLAYER_SOUND_FILE = 'Main Game\Sound Effects\ding-idea-40142_ljQkHJgG.wav'  # Custom player move sound file
 AI_SOUND_FILE = 'Main Game\Sound Effects\mixkit-arcade-robot-sound-1415_OiPFfAGb.wav'  # Custom AI sound file
 WINNER_SOUND_FILE = 'Main Game\Sound Effects\mixkit-winning-chimes-2015.wav'  # Custom winner sound file
+WIN_SCORE = 1000
+LOSS_SCORE = -1000
+INFINITY = float('inf')
 
 # Initialize Pygame
 pygame.init()
@@ -56,9 +59,9 @@ def draw_grid():
             pygame.draw.rect(window, (0, 0, 0), rect, 1)  # Draw grid lines
 
             if grid[row][col] == 'player':
-                pygame.draw.rect(window, PLAYER_COLOR, rect)  # Draw player's square
+                pygame.draw.rect(window, PLAYER_COLOUR, rect)  # Draw player's square
             elif grid[row][col] == 'ai':
-                pygame.draw.rect(window, AI_COLOR, rect)  # Draw AI's square
+                pygame.draw.rect(window, AI_COLOUR, rect)  # Draw AI's square
 
     pygame.display.update()
 
@@ -83,11 +86,35 @@ def check_winner():
                         return grid[row][col]
                     if all(grid[row + i][col - i] == grid[row][col] for i in range(4)):
                         return grid[row][col]
-
+                    
     return None
 
 def ai_make_move():
     # AI's move (intelligent placement)
+
+    # Check if AI can win
+    for row in range(GRID_SIZE):
+        for col in range(GRID_SIZE):
+            if is_valid_move(row, col):
+                grid[row][col] = 'ai'
+                if check_winner() == 'ai':
+                    grid[row][col] = 'ai'
+                    ai_sound.play()  # Play the AI move sound
+                    return
+                grid[row][col] = None
+
+    # Check if AI can block player's line of 4
+    for row in range(GRID_SIZE):
+        for col in range(GRID_SIZE):
+            if is_valid_move(row, col):
+                grid[row][col] = 'player'
+                if check_winner() == 'player':
+                    grid[row][col] = 'ai'
+                    ai_sound.play()  # Play the AI move sound
+                    return
+                grid[row][col] = None
+
+    # Otherwise, evaluate the best move as before
     best_move = None
     best_score = float('-inf')
 
@@ -104,68 +131,97 @@ def ai_make_move():
         grid[row][col] = 'ai'
         ai_sound.play()  # Play the AI move sound
 
-def evaluate_move(row, col, player, opponent, blocking=False): # Evaluate the move
+def evaluate_move(row, col, player, opponent, blocking=False):
     score = 0
 
     # Check horizontal line
-    for c in range(col, col + 4):
-        if c < GRID_SIZE:
-            if grid[row][c] == player:
-                score += 1
-            elif grid[row][c] == opponent:
+    for c in range(col - 3, col + 1):
+        if c >= 0 and c + 4 < GRID_SIZE:
+            tiles = [grid[row][c+i] for i in range(5)]
+            if tiles.count(opponent) == 4:
                 if blocking:
-                    score += 2  # Give higher weight for blocking the player's moves
-                else:
-                    score -= 1
+                    # Block the player's line of 4 by placing a tile in the direction of the line of 3
+                    if c - 1 >= 0 and grid[row][c - 1] is None:
+                        score += 10
+                    elif c + 5 < GRID_SIZE and grid[row][c + 5] is None:
+                        score += 10
 
     # Check vertical line
-    for r in range(row, row + 4):
-        if r < GRID_SIZE:
-            if grid[r][col] == player:
-                score += 1
-            elif grid[r][col] == opponent:
+    for r in range(row - 3, row + 1):
+        if r >= 0 and r + 4 < GRID_SIZE:
+            tiles = [grid[r+i][col] for i in range(5)]
+            if tiles.count(opponent) == 4:
                 if blocking:
-                    score += 2  # Give higher weight for blocking the player's moves
-                else:
-                    score -= 1 
+                    # Block the player's line of 4 by placing a tile in the direction of the line of 3
+                    if r - 1 >= 0 and grid[r - 1][col] is None:
+                        score += 10
+                    elif r + 5 < GRID_SIZE and grid[r + 5][col] is None:
+                        score += 10
 
     # Check diagonal line (top-left to bottom-right)
-    for i in range(4):
-        r = row + i
-        c = col + i
-        if r < GRID_SIZE and c < GRID_SIZE:
-            if grid[r][c] == player:
-                score += 1
-            elif grid[r][c] == opponent:
+    for i in range(5):
+        r = row - i
+        c = col - i
+        if r >= 0 and c >= 0 and r + 4 < GRID_SIZE and c + 4 < GRID_SIZE:
+            tiles = [grid[r+i][c+i] for i in range(5)]
+            if tiles.count(opponent) == 4:
                 if blocking:
-                    score += 2  # Give higher weight for blocking the player's moves
-                else:
-                    score -= 1
+                    # Block the player's line of 4 by placing a tile in the direction of the line of 3
+                    if r - 1 >= 0 and c - 1 >= 0 and grid[r - 1][c - 1] is None:
+                        score += 10
+                    elif r + 5 < GRID_SIZE and c + 5 < GRID_SIZE and grid[r + 5][c + 5] is None:
+                        score += 10
 
     # Check diagonal line (top-right to bottom-left)
-    for i in range(4):
-        r = row + i
-        c = col - i
-        if r < GRID_SIZE and c >= 0:
-            if grid[r][c] == player:
-                score += 1
-            elif grid[r][c] == opponent:
+    for i in range(5):
+        r = row - i
+        c = col + i
+        if r >= 0 and c < GRID_SIZE and r + 4 < GRID_SIZE and c - 4 >= 0:
+            tiles = [grid[r+i][c-i] for i in range(5)]
+            if tiles.count(opponent) == 4:
                 if blocking:
-                    score += 2  # Give higher weight for blocking the player's moves
-                else:
-                    score -= 1
+                    # Block the player's line of 4 by placing a tile in the direction of the line of 3
+                    if r - 1 >= 0 and c + 1 < GRID_SIZE and grid[r - 1][c + 1] is None:
+                        score += 10
+                    elif r + 5 < GRID_SIZE and c - 5 >= 0 and grid[r + 5][c - 5] is None:
+                        score += 10
 
     return score
+
+def minimax(depth, maximizing_player):
+    if depth == 0 or check_winner():
+        return 0
+
+    if maximizing_player:
+        max_eval = float('-inf')
+        for row in range(GRID_SIZE):
+            for col in range(GRID_SIZE):
+                if is_valid_move(row, col):
+                    grid[row][col] = 'ai'
+                    eval = minimax(depth - 1, False)
+                    grid[row][col] = None
+                    max_eval = max(max_eval, eval)
+        return max_eval
+    else:
+        min_eval = float('inf')
+        for row in range(GRID_SIZE):
+            for col in range(GRID_SIZE):
+                if is_valid_move(row, col):
+                    grid[row][col] = 'player'
+                    eval = minimax(depth - 1, True)
+                    grid[row][col] = None
+                    min_eval = min(min_eval, eval)
+        return min_eval
 
 def display_winner(winner):
     global winner_sound_played
 
     if winner == 'player':
-        text = custom_font.render("Player wins!", True, FONT_COLOR)
+        text = custom_font.render("Player wins!", True, FONT_COLOUR)
     elif winner == 'ai':
-        text = custom_font.render("AI wins!", True, FONT_COLOR)
+        text = custom_font.render("AI wins!", True, FONT_COLOUR)
     else:
-        text = custom_font.render("It's a tie!", True, FONT_COLOR)
+        text = custom_font.render("It's a tie!", True, FONT_COLOUR)
 
     text_rect = text.get_rect(center=(WINDOW_SIZE[0] // 2, PADDING_TOP // 2))
     window.blit(text, text_rect)
@@ -238,7 +294,8 @@ while True:
             break
 
         display_winner(winner) # Display the winner
-        text = custom_font.render("Press 'R' to play again", True, FONT_COLOR_2) # Display the restart message
+        text = custom_font.render("Press 'R' to play again", True, FONT_COLOUR_2) # Display the restart message
         text_rect = text.get_rect(center=(WINDOW_SIZE[0] // 2, WINDOW_SIZE[1] - (PADDING_TOP // 2))) # Center the text
         window.blit(text, text_rect) # Display the text
         pygame.display.update() # Update the display
+
